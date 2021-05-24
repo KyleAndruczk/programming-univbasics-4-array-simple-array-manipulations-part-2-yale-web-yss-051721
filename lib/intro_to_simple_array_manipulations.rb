a8e6e1df15d4e59aa3512d32bc23737b1db42610
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-24 23:06:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,15 @@
-using_concat(arr1, arr2)
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end 
+
+def using_flatten(arr)
+  arr.flatten 
+end
+
+def using_delete(arr, str)
+  arr.delete(str)
+end 
